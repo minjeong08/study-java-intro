a9bccf094f3457c79089434caf0226ec3a2feb6a
minnjung0887921 <minjeong0887921@gmail.com>
2023-12-04 19:56:59 +0900

    예제

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,12 @@
+package variable.ex;
+
+public class VarEx3 {
+
+    public static void main(String[] args) {
+        long longVar = 10000000000L;
+        boolean booleanVar = true;
+
+        System.out.println("longVar = " + longVar);
+        System.out.println("booleanVar = " + booleanVar);
+    }
+}
