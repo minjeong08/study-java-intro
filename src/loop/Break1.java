93ad1f678b27447530dba1ee7a19e33eb5febaa4
minnjung0887921 <minjeong0887921@gmail.com>
2023-12-05 21:07:51 +0900

    break, continue

---
+++ src/loop/Break1.java
@@ -0,0 +1,18 @@
+package loop;
+
+public class Break1 {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 1;
+
+        while(true) {
+            sum += i;
+            if (sum > 10) {
+                System.out.println("i : " + i);
+                break;
+            }
+            i++;
+        }
+    }
+}
