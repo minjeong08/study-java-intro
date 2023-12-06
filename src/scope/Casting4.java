ca857542f236b8c02ffb043c39ecfeaa0afff0c5
minnjung0887921 <minjeong0887921@gmail.com>
2023-12-06 17:34:45 +0900

    계산과 형변환

---
+++ src/scope/Casting4.java
@@ -0,0 +1,23 @@
+package scope;
+
+public class Casting4 {
+
+    public static void main(String[] args) {
+        int div1 = 3/2; // 1.5
+        System.out.println("div1 = " + div1); // 1
+
+        double div2 = 3/2; // 1.5
+        System.out.println("div2 = " + div2); // 1.0
+
+        double div3 = 3.8/2; // 1.9
+        System.out.println("div3 = " + div3); // 1.9
+
+        double div4 = (double) 3/2; // 1.5
+        System.out.println("div4 = " + div4); // 1.5
+
+        int a = 3;
+        int b = 2;
+        double result = (double) a/b;
+        System.out.println("result = " + result);
+    }
+}
