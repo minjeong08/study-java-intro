8e99f6dea6ffe026a714e2a4cfdf3ce20d33b031
minnjung0887921 <minjeong0887921@gmail.com>
2023-12-06 17:28:22 +0900

    명시적 형변환

---
+++ src/scope/Casting2.java
@@ -0,0 +1,13 @@
+package scope;
+
+public class Casting2 {
+
+    public static void main(String[] args) {
+        double doubleValue = 1.5;
+        int intValue;
+
+//        intValue = doubleValue; // 컴파일 오류 발생
+        intValue = (int) doubleValue; // 형변환
+        System.out.println("intValue = " + intValue);
+    }
+}
