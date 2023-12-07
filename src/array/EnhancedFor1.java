854ef0237d2d8dffe93ce25f7088eeb429cfd84b
minnjung0887921 <minjeong0887921@gmail.com>
2023-12-07 17:46:40 +0900

    향상된 for문

---
+++ src/array/EnhancedFor1.java
@@ -0,0 +1,13 @@
+package array;
+
+public class EnhancedFor1 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5};
+
+        // 일반 for문
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
