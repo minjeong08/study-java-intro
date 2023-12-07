51f49c6f94e9cd3a012a609611fc0224390b00ba
minnjung0887921 <minjeong0887921@gmail.com>
2023-12-07 17:39:19 +0900

    2차원 배열

---
+++ src/array/ArrayDi3.java
@@ -0,0 +1,24 @@
+package array;
+
+public class ArrayDi3 {
+
+    public static void main(String[] args) {
+        // 2x3 2차원 배열을 만든다.
+        int[][] arr = new int[3][5];
+
+        int i = 1;
+        for (int row = 0; row < arr.length ; row++) {
+            for (int col = 0; col < arr[row].length ; col++) {
+                arr[row][col] = i++;
+            }
+            System.out.println();
+        }
+
+        for (int row = 0; row < arr.length ; row++) {
+            for (int col = 0; col < arr[row].length ; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
